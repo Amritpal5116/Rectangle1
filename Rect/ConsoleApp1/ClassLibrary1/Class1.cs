24519fa8b1814c85978498075f26989c3c6cd031
Amritpal5116 <51034116+Amritpal5116@users.noreply.github.com>
2019-05-29 05:35:57 -0400

    Add files via upload

---
+++ Rect/ConsoleApp1/ClassLibrary1/Class1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using rectangleabc;
+using NUnit.Framework;
+
+namespace testcase_assignment
+{
+    [TestFixture]
+    class testcase
+    {
+        [Test]
+        public void GetLength_input2_expectedLengthEquals2()
+        {
+            //Arrange
+            int l = 2;
+            int w = 8;
+            Rectangle testrectangle = new Rectangle(l, w);
+
+            //Act
+            int length = testrectangle.GetLength();
+
+            //Assert
+            Assert.AreEqual(l, length);
+        }
+
+        [Test]
+        public void SetLength_input6_expectedLengthEquals6()
+        {
+            //Arrange
+            int l = 6;
+            int w = 1;
+            Rectangle testrectangle = new Rectangle(l, w);
+
+            //Act
+            int length = testrectangle.SetLength(l);
+
+            //Assert
+            Assert.AreEqual(l, length);
+        }
+
+
+
+        [Test]
+        public void GetWidth_input7_expectedLengthEquals7()
+        {
+            //Arrange
+            int l = 4;
+            int w = 7;
+            Rectangle testrectangle = new Rectangle(l, w);
+
+            //Act
+            int width = testrectangle.GetWidth();
+
+            //Assert
+            Assert.AreEqual(w, width);
+        }
+
+        [Test]
+        public void SetWidth_input8_expectedLengthEquals8()
+        {
+            //Arrange
+            int l = 9;
+            int w = 8;
+            Rectangle testrectangle = new Rectangle(l, w);
+
+            //Act
+            int width = testrectangle.SetWidth(w);
+
+            //Assert
+            Assert.AreEqual(w, width);
+        }
+
+        [Test]
+        public void GetPerimeter_inputlength3andwidth3_expectedperimeterEquals12()
+        {
+            //Arrange
+            int l = 3;
+            int w = 3;
+
+            int expectedResult = (l * 2) + (w * 2);
+
+            Rectangle testrectangle = new Rectangle(l, w);
+
+            //Act
+            int actualResult = testrectangle.GetPerimeter();
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void GetArea_inputlength9andwidth4_expectedAreaEquals36()
+        {
+            //Arrange
+            int l = 9;
+            int w = 4;
+
+            int expectedResult = (l * w);
+
+            Rectangle testrectangle = new Rectangle(l, w);
+
+            //Act
+            int actualResult = testrectangle.GetArea();
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+    }
+}
+
--- Rect/ConsoleApp1/ConsoleApp1/Rect.cs
+++ Rect/ConsoleApp1/ConsoleApp1/Rect.cs
@@ -4,9 +4,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace rectangleabc
+
 {
-    class Rect
+    public class Rect
     {
+        private int length;
+        private int width;
+
+        public Rect()
+        {
+            length = 1;
+            width = 1;
+        }
+
+        public Rect(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+
+        public int GetLength()
+        {
+            return length;
+        }
+
+
+        public int SetLength(int length)
+        {
+            this.length = length;
+            return this.length;
+        }
+
+
+        public int GetWidth()
+        {
+            return width;
+
+        }
+
+
+
+        public int SetWidth(int width)
+        {
+            this.width = width;
+            return this.width;
+        }
+
+
+
+        public int GetPerimeter()
+        {
+            return (width * 2) + (length * 2);
+        }
+
+
+
+        public int GetArea()
+        {
+            return length * width;
+        }
+
     }
 }
+
+
+
+
+
